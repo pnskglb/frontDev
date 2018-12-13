bf6731ccf15bd4a5ecbd63948d3834e3b86cebe5
pnskglb <45828863+pnskglb@users.noreply.github.com>
2018-12-13 17:20:50 +0300

    button switch-lang #7

---
+++ consuLanding/site/js/changeLang.js
@@ -0,0 +1,13 @@
+
+     function changeLang()
+     {
+
+        var body = document.getElementsByTagName("body")[0];
+          body.className= "en";
+     }
+     function changeLang2()
+     {
+
+        var body = document.getElementsByTagName("body")[0];
+          body.className= "ru";
+     }
